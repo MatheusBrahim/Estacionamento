38a6198e0062e7f73b0c790b0418c1b76a6c7a0d
MatheusBrahim <matheusbrahim.face@gmail.com>
2025-05-18 02:58:29 -0300

    Resolvido

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
-﻿double vlEst, vlValet, vlLavagem, tempPerm, total;
-int tempo, horas, hrsAdd;
+﻿decimal vlEst, vlValet, vlLavagem, total, horas, tempo;
 string tamVeiculo, servValet, servLavagem;
 
 Console.Clear();
 
 titulo();
-
 valiVeiculo();
-
+tempoHrs();
 valiValet();
-
 valiLavagem();
+valorEstacionamento();
+valorValet();
+valorLavagem();
+valorTotal();
 
+void titulo()
+{
+    Console.WriteLine("--- Estacionamento ---");
+}
 
 string valiVeiculo ()
 {
@@ -21,9 +26,28 @@ string valiVeiculo ()
     if (tamVeiculo != "P" && tamVeiculo != "G")
     {
         Console.WriteLine("--- Dados Invalidos ---");
+        return valiVeiculo();
     }
 
     return tamVeiculo;
+}
+
+int tempoHrs()
+{
+    Console.Write("Tempo de permanência (min)...: ");
+    tempo = Convert.ToInt32(Console.ReadLine());
+
+    if (tempo >= 720)
+    {
+        Console.WriteLine("--- Dados Invalidos ---");
+        return tempoHrs();
+    }
+    else
+    {
+        horas = (int)(tempo / 60);
+    }
+
+    return (int)horas;
 }
 
 string valiValet ()
@@ -31,9 +55,11 @@ string valiValet ()
     Console.Write("Serviço de valet (S/N).......: ");
     servValet = Console.ReadLine().ToUpper();
 
-    if (servValet != "S" && tamVeiculo != "N")
+    if (servValet != "S" && servValet != "N")
     {
         Console.WriteLine("--- Dados Invalidos ---");
+        return valiValet();
+
     }
 
     return servValet;
@@ -44,15 +70,71 @@ string valiLavagem ()
     Console.Write("Serviço de lavagem (S/N).....: ");
     servLavagem = Console.ReadLine().ToUpper();
 
-    if (servLavagem != "S" && tamVeiculo != "N")
+    if (servLavagem != "S" && servLavagem != "N")
     {
         Console.WriteLine("--- Dados Invalidos ---");
+        return valiLavagem();
     }
+
+    Console.WriteLine();
 
     return servLavagem;
 }
 
-void titulo()
+Decimal valorEstacionamento()
 {
-    Console.WriteLine("--- Estacionamento ---");
+    if (horas >= 5 && tamVeiculo == "G")
+    {
+        vlEst = 80;
+    }
+    else if (horas >= 5 && tamVeiculo == "P")
+    {
+        vlEst = 50;
+    }
+    else
+    {
+        vlEst = tamVeiculo == "G" ? 20 + (20 * (horas - 1)) : 20 + (10 * (horas - 1));
+    }
+    Console.WriteLine($"Estacionamento..:       R$ {vlEst:N2}");
+    return vlEst;
+}
+
+Decimal valorValet()
+{
+    vlValet = servValet == "S" ? vlEst * 0.2m : 0;
+    Console.WriteLine($"Valet...........:       R$ {vlValet:N2}");
+    return vlValet;
+}
+
+Decimal valorLavagem()
+{
+    if (tamVeiculo == "G")
+    {
+        vlLavagem = 100;
+    }
+    else
+    {
+        vlLavagem = 50;
+    }
+
+    if (servLavagem != "S")
+    {
+        Console.WriteLine("Lavagem.........:       R$ 0,00");
+    }
+    else
+    {
+        Console.WriteLine($"Lavagem.........:       R$ {vlLavagem:N2}");
+    }
+
+    return vlLavagem;
+}
+
+decimal valorTotal()
+{
+    Console.WriteLine("--------------------------------");
+
+    total = servLavagem == "S" ? vlEst + vlValet + vlLavagem : vlEst + vlValet;
+    Console.WriteLine($"Total...........:       R$ {total:N2}");
+
+    return total;
 }
